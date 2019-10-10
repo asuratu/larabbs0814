c7d20eef7d4f85767d03eafcbf16d37e59381860
Antony <455224159@qq.com>
2019-10-10 19:29:03 +0800

    集成 administrator

--- config/app.php
+++ config/app.php
@@ -166,6 +166,8 @@ return [
         /*
          * Package Service Providers...
          */
+        Frozennode\Administrator\AdministratorServiceProvider::class,
+
 
         /*
          * Application Service Providers...
